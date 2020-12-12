bb159d00c68b2a12b2c665f8e9f64c945cf178dc
Adam Kariv <adam.kariv@gmail.com>
2020-12-12 14:05:48 +0200

    dist

--- projects/hatool/src/lib/script-runner-types.ts
+++
@@ -1,7 +0,0 @@
-export type Meta = {
-    key: string,
-    value: any
-  }[];
-export type CBType = (key: string, value: any, record?: any) => void;
-export type MetaCBType = (meta: Meta) => void;
-export type EventCBType = (event: string) => void;
